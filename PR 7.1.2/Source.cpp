c7ae383b3ff33189305c796a710ee6b90add1342
Viktor <vbonchykv@gmail.com>
2021-02-24 14:42:11 +0200

    first master commit

---
+++ PR 7.1.2/Source.cpp
@@ -0,0 +1,78 @@
+// Lab_7_1.cpp
+// Обухова Віктора
+// Лабораторна робота No 7.1.2 ітерація
+// Пошук заданих елементів та впорядкування рядків / стовпчиків матриці.
+// Варіант 7
+
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High);
+void Print(int** a, const int rowCount, const int colCount);
+void Sort(int** a, const int rowCount, const int colCount);
+void Change(int** a, const int row1, const int row2, const int colCount);
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Low = 4;
+	int High = 51;
+	int rowCount = 8;
+	int colCount = 5;
+	int** a = new int* [rowCount];
+	for (int i = 0; i < rowCount; i++)
+		a[i] = new int[colCount];
+	Create(a, rowCount, colCount, Low, High);
+	Print(a, rowCount, colCount);
+	Sort(a, rowCount, colCount);
+	Print(a, rowCount, colCount);
+
+	for (int i = 0; i < rowCount; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+}
+void Create(int** a, const int rowCount, const int colCount, const int Low,
+	const int High)
+{
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			a[i][j] = Low + rand() % (High - Low + 1);
+}
+void Print(int** a, const int rowCount, const int colCount)
+{
+	cout << endl;
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+			cout << setw(4) << a[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+void Sort(int** a, const int rowCount, const int colCount)
+{
+	for (int i0 = 1; i0 < rowCount + 1; i0++)
+		for (int i1 = 0; i1 < rowCount - i0 - 1; i1++)
+			if ((a[i1][0] > a[i1 + 1][0])
+				||
+				(a[i1][0] == a[i1 + 1][0] &&
+				 a[i1][1] < a[i1 + 1][1])
+				||
+				(a[i1][0] == a[i1 + 1][0] &&
+			 	 a[i1][1] == a[i1 + 1][1] &&
+			 	 a[i1][3] < a[i1 + 1][3]))
+			Change(a, i1, i1 + 1, colCount);
+ }
+
+void Change(int** a, const int row1, const int row2, const int colCount)
+{
+	int tmp;
+	for (int j = 0; j < colCount; j++)
+	{
+		tmp = a[row1][j];
+		a[row1][j] = a[row2][j];
+		a[row2][j] = tmp;
+	}
+}
